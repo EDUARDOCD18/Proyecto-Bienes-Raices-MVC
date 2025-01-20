e00b3d36c09f48a2cece3bd58ae4cb176f5444fb
eduardocd18 <javierecd@gmail.com>
2025-01-20 11:14:04 -0400

    395. Registrando URL's y los métodos asociados a ellas

--- Router.php
+++ Router.php
@@ -1,10 +1,34 @@
-<?php 
+<?php
 
 
 namespace MVC;
 
-class Router {
-    public function __construct(){
-        echo "Creando el Router";
+class Router
+{
+
+    public $rutasGET = [];
+    public $rutasPOST = [];
+
+    public function get($url, $fn)
+    {
+        $this->rutasGET[$url] = $fn;
+    }
+
+    public function comprobarRutas()
+    {
+        $urlActual = $_SERVER['PATH_INFO'] ?? '/';
+        $metedo = $_SERVER['REQUEST_METHOD'];
+
+        if ($metedo === 'GET') {
+            $fn = $this->rutasGET[$urlActual] ?? null;
+        }
+
+
+        if ($fn) {
+            // Si la URL existe
+            call_user_func($fn, $this);
+        } else {
+            echo "404";
+        }
     }
 }
--- public/index.php
+++ public/index.php
@@ -4,4 +4,11 @@ require_once __DIR__ .  '/../includes/app.php';
 
 use MVC\Router;
 
-$router = new Router(); 
+$router = new Router();
+
+$router->get('/nosotros', 'funcion_nosotros');
+$router->get('/vendedor', 'vendedor');
+$router->get('/tienda', 'tienda');
+$router->get('/admin', 'funcion_admin');
+
+$router->comprobarRutas();
