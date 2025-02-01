867a1f6c5ab52f1d645e76ad8c281783b180b6d2
eduardocd18 <javierecd@gmail.com>
2025-02-01 16:54:36 -0400

    403. Modificando el código para almacenar imagenes en la carpeta publica

--- controllers/PropiedadController.php
+++ controllers/PropiedadController.php
@@ -5,6 +5,9 @@ namespace Controllers;
 use MVC\Router;
 use Model\Propiedad;
 use Model\Vendedor;
+//Importar Intervention Image
+use Intervention\Image\ImageManager as Image;
+use Intervention\Image\Drivers\Gd\Driver;
 
 class PropiedadController
 {
@@ -14,7 +17,8 @@ class PropiedadController
 
         $propiedades = Propiedad::all();
         $vendedores = Vendedor::all();
-        $resultado = null;
+        // Muestra mensaje condicional 
+        $resultado = $_GET['resultado'] ?? null;
 
         $router->render('propiedades/admin', [
             'propiedades' => $propiedades,
@@ -27,15 +31,48 @@ class PropiedadController
 
         $propiedad = new Propiedad;
         $vendedores = Vendedor::all();
+        // Arreglo con los mensajes de errores
+        $errores = Propiedad::getErrores();
 
-        if($_SERVER['REQUEST_METHOD'] === 'POST') {
-            debuguear('hola');
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // Instancia el objeto
+            $propiedad = new Propiedad($_POST['propiedad']);
+
+            // Generar un nombre único para cada imagen
+            $nombreImagen = md5(uniqid(rand(), true)) . ".jpg";
+
+            // Setea la imagen
+            // Realizar un resize con Intervention Image
+            if ($_FILES['propiedad']['tmp_name']['imagen']) {
+                $manager = new Image(Driver::class);
+                $imagenSubir = $manager->read($_FILES['propiedad']['tmp_name']['imagen'])->cover(800, 600);
+                $propiedad->setImage($nombreImagen);
+            }
+
+            // Validar
+            $errores = $propiedad->validar();
+
+            // Revisar que el arrglo de errores esté vacío
+            if (empty($errores)) {
+
+                // Crear carpeta
+                if (!is_dir(CARPETA_IMAGENES)) {
+                    mkdir(CARPETA_IMAGENES);
+                }
+
+                // Guarda la imagen
+                $imagenSubir->save(CARPETA_IMAGENES . $nombreImagen);
+
+                $resultado = $propiedad->guardar(); // Llamar función para guardar en la BD
+
+            }
         }
 
 
         $router->render('propiedades/crear', [
             'propiedad' => $propiedad,
-            'vendedores' => $vendedores
+            'vendedores' => $vendedores,
+            'errores' => $errores
         ]);
     }
     public static function actualizar()
