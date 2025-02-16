b80f918ad563a4a7181965deff6cca04a47fff6b
eduardocd18 <javierecd@gmail.com>
2025-02-16 16:57:00 -0400

    408. Creando el Controlador para Vendedores

---
+++ controllers/VendedorController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Controllers;
+
+class VendedorController
+{
+    /* CREAR UN REGISTRO */
+    public static function crear()
+    {
+        echo "crear";
+    }
+    /* ACTUALIZAR UN REGISTRO */
+    public static function actualizar()
+    {
+        echo "ACT";
+    }
+    /* ELIMINAR UN REGISTRO */
+    public static function eliminar()
+    {
+        echo "BORRAR";
+    }
+}
--- public/index.php
+++ public/index.php
@@ -4,9 +4,12 @@ require_once __DIR__ .  '/../includes/app.php';
 
 use MVC\Router;
 use Controllers\PropiedadController;
+use Controllers\VendedorController;
 
 $router = new Router();
 
+
+/* PROPIEDADES */
 // GET
 $router->get('/admin', [PropiedadController::class, 'index']);
 $router->get('/propiedades/crear', [PropiedadController::class, 'crear']);
@@ -15,5 +18,14 @@ $router->get('/propiedades/actualizar', [PropiedadController::class, 'actualizar
 $router->post('/propiedades/crear', [PropiedadController::class, 'crear']);
 $router->post('/propiedades/actualizar', [PropiedadController::class, 'actualizar']);
 $router->post('/propiedades/eliminar', [PropiedadController::class, 'eliminar']);
+
+/* VENDEDORES */
+// GET
+$router->get('/vendedores/crear', [VendedorController::class, 'crear']);
+$router->get('/vendedores/actualizar', [VendedorController::class, 'actualizar']);
+// POST
+$router->post('/vendedores/crear', [VendedorController::class, 'crear']);
+$router->post('/vendedores/actualizar', [VendedorController::class, 'actualizar']);
+$router->post('/vendedores/eliminar', [VendedorController::class, 'eliminar']);
 
 $router->comprobarRutas();
