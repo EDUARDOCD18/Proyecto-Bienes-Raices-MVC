51cd5e4c4ea2ac1e43aeafe46b88ca6e669e2f4f
eduardocd18 <javierecd@gmail.com>
2025-02-22 07:11:43 -0400

    410. Validación de Vendedores

--- controllers/VendedorController.php
+++ controllers/VendedorController.php
@@ -10,8 +10,22 @@ class VendedorController
     /* CREAR UN REGISTRO */
     public static function crear(Router $router)
     {
-        $vendedor = new Vendedor; 
+        $vendedor = new Vendedor;
         $errores = Vendedor::getErrores();
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // Instancia el objeto
+            $propiedad = new Vendedor($_POST['vendedor']);
+
+            // Validar
+            $errores = $propiedad->validar();
+
+            // Revisar que el arrglo de errores esté vacío
+            if (empty($errores)) {
+                $resultado = $propiedad->guardar(); // Llamar función para guardar en la BD
+            }
+        }
+
         $router->render('vendedores/crear', [
             'errores' => $errores,
             'vendedor' => $vendedor
