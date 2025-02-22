41c70ff1498897bc4f6c4b43eb2e19b563f258ab
eduardocd18 <javierecd@gmail.com>
2025-02-22 19:41:47 -0400

    414. Creando las URL's para las páginas

--- public/index.php
+++ public/index.php
@@ -2,9 +2,11 @@
 
 require_once __DIR__ .  '/../includes/app.php';
 
+use Controllers\PaginasController;
 use MVC\Router;
 use Controllers\PropiedadController;
 use Controllers\VendedorController;
+use Controllers\PaginasControllers;
 
 $router = new Router();
 
@@ -27,5 +29,15 @@ $router->get('/vendedores/actualizar', [VendedorController::class, 'actualizar']
 $router->post('/vendedores/crear', [VendedorController::class, 'crear']);
 $router->post('/vendedores/actualizar', [VendedorController::class, 'actualizar']);
 $router->post('/vendedores/eliminar', [VendedorController::class, 'eliminar']);
+
+/* PARA VISITANTES */
+$router->get('/', [PaginasController::class, 'index']);
+$router->get('/nosotros', [PaginasController::class, 'nosotros']);
+$router->get('/propiedades', [PaginasController::class, 'propiedades']);
+$router->get('/propiedad', [PaginasController::class, 'propiedad']);
+$router->get('/blog', [PaginasController::class, 'blog']);
+$router->get('/entrada', [PaginasController::class, 'entrada']);
+$router->get('/contacto', [PaginasController::class, 'contacto']);
+$router->post('/contacto', [PaginasController::class, 'contacto']);
 
 $router->comprobarRutas();
